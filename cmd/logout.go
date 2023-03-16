5f91d973c3c4fd80f3f1d9009ac2efcfe0957497
Saahil Bhavsar <saahil_bhavsar@outlook.com>
2023-03-16 21:15:21 +0530

    Adds command description and updates the logging behaviour

--- cmd/logout.go
+++ cmd/logout.go
@@ -1,10 +1,10 @@
 package cmd
 
 import (
+	"github.com/XanderWatson/iitj-autoproxy/pkg"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"log"
-	"net/http"
 )
 
 func init() {
@@ -13,24 +13,19 @@ func init() {
 
 var logoutCmd = &cobra.Command{
 	Use:   "logout",
-	Short: "",
-	Long:  "",
+	Short: "Logout of your firewall authentication",
+	Long:  "Logout of your firewall authentication",
 	Run: func(cmd *cobra.Command, args []string) {
 		token := viper.GetString("token")
 
 		if token == "" {
-			cobra.CheckErr("User not logged in")
+			pkg.Logger.Println("User not logged in")
+			log.Println("User not logged in")
 		}
 
-		cobra.CheckErr(Logout(token))
+		cobra.CheckErr(pkg.Logout(token))
+
+		pkg.Logger.Println("Logged out successfully")
 		log.Println("Logged out successfully")
 	},
-}
-
-func Logout(token string) error {
-	url := viper.GetString("login_base_url") + "logout?" + token
-
-	_, err := http.Get(url)
-
-	return err
 }
