630b469f140d4058f6ad63452647c649284e4b99
Saahil Bhavsar <saahil_bhavsar@outlook.com>
2023-08-27 06:06:02 +0530

    Minor changes

--- proxy.sh
+++ proxy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-AUTOPROXY_DIR=/path/to/iitj-autoproxy
+AUTOPROXY_DIR=/usr/bin
 
 nohup $AUTOPROXY_DIR/autoproxyd & > $AUTOPROXY_DIR/autoproxyd.log
 sleep 3
-$AUTOPROXY_DIR/autoproxy-cli login
+$AUTOPROXY_DIR/autoproxy login
 sleep 5
 sudo killall autoproxyd
